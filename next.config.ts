560b9485c34ee64bce7f118ff260aa3ee51e3a72
Ricardo Valero <ricardo.valero@hitowerit.mx>
2024-11-05 11:03:09 -0600

    update comments

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,15 @@
-import { NextConfig } from 'next'
+import { NextConfig } from "next";
 
 const config: NextConfig = {
-    // Ignore TypeScript errors during builds temporarily
+    // TODO: Ignore TypeScript errors during builds temporarily
     typescript: {
         ignoreBuildErrors: true,
     },
     // Ignore ESBuild during builds because Biome is not configurable with Next.js
-    // Support for alternative linters https://github.com/vercel/next.js/discussions/59347
+    // TODO: Support for alternative linters https://github.com/vercel/next.js/discussions/59347
     eslint: {
         ignoreDuringBuilds: true,
     },
-}
+};
 
-export default config
+export default config;
--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
-// Support for postcss typescript config https://github.com/vercel/next.js/pull/69827
+// TODO: Support for postcss typescript config https://github.com/vercel/next.js/pull/69827
 module.exports = {
     plugins: {
         tailwindcss: {},
         autoprefixer: {},
     },
-}
+};
