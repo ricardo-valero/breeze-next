7c5c9e07a6b8b1caa7e73152c1837db4151e8267
Ricardo Valero <ricardo.valero@hitowerit.mx>
2024-11-05 09:54:06 -0600

    tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,9 @@
-module.exports = {
+import { Config } from 'tailwindcss'
+
+const config: Config = {
     content: ['./src/**/*.{js,jsx,ts,tsx}'],
     theme: {},
     plugins: [require('@tailwindcss/forms')],
 }
+
+export default config
