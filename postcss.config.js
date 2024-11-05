5332fc06e9d290d245b74bb3e6b400b5e8a8b2a2
Ricardo Valero <ricardo.valero@hitowerit.mx>
2024-11-05 10:37:02 -0600

    remove and update packages

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
+// Support for postcss typescript config https://github.com/vercel/next.js/pull/69827
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+    plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+    },
 }
